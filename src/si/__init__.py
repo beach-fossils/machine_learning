c5125316a5540f9e7ff17ed38701c05522ea31c0
beach-fossils <jddmoura@gmail.com>
2022-09-26 11:45:43 +0100

    Adding my co-authorship to README.md file

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,6 +1,6 @@
-__author__ = "Fernando Cruz"
+__author__ = "josé diogo moura"
 __credits__ = ["Vítor Pereira, Fernando Cruz"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Fernando Cruz"
-__email__ = "fernando.cruz@ceb.uminho.pt"
+__maintainer__ = "josé diogo moura"
+__email__ = "pg45965@alunos.uminho.pt"
