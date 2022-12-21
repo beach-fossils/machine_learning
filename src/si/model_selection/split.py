630c003813690c8276d401b84f8350af25f3cc0c
beach-fossils <jddmoura@gmail.com>
2022-12-21 00:27:49 +0000

    [ADD] tested and working

--- src/si/model_selection/split.py
+++ src/si/model_selection/split.py
@@ -11,7 +11,7 @@ def train_test_split(dataset, test_size: float = 0.2, random_state=None) -> Tupl
         np.random.seed(random_state)
 
     # get dataset size
-    n_samples = len(dataset)  # n_samples é o número de amostras do dataset
+    n_samples = dataset.shape()[0]  # n_samples é o número de amostras do dataset
 
     # get number of samples in the test set
     n_test_samples = int(n_samples * test_size)  # n_test_samples é o número de amostras do dataset de teste
@@ -27,12 +27,30 @@ def train_test_split(dataset, test_size: float = 0.2, random_state=None) -> Tupl
     train_indexes = permutation[n_test_samples:]  # train_indexes é um array com os índices das amostras do dataset de
     # treino
 
-    # create train and test sets
-    train_set = Dataset(dataset.X[train_indexes], dataset.y[train_indexes],
-                        features=dataset.features,
-                        label=dataset.label)  # train_set é o dataset de treino, com as amostras de train_indexes
-    test_set = Dataset(dataset.X[test_indexes], dataset.y[test_indexes],
-                       features=dataset.features, label=dataset.label)  # test_set é o dataset de teste, features é
-    # o nome das features e label é o nome da label
+    # create train and test sets it is giving me error of NoneType in the next line
+    train_set = Dataset(X=dataset.X[train_indexes], y=dataset.y[train_indexes], features=dataset.features,
+                        label=dataset.label)
+    test_set = Dataset(X=dataset.X[test_indexes], y=dataset.y[test_indexes], features=dataset.features,
+                       label=dataset.label)
 
     return train_set, test_set
+
+
+# testing the function
+if __name__ == '__main__':
+    from src.si.io.CSV import read_csv
+
+    # load the dataset
+    dataset = read_csv('/Users/josediogomoura/machine_learning/datasets/datasets/iris.csv', sep=',', features=True,
+                       label=True)
+
+    # split the dataset
+    train_set, test_set = train_test_split(dataset, test_size=0.2, random_state=42)
+
+    # print the train set
+    print(train_set.X)
+    print(train_set.y)
+
+    # print the test set
+    print(test_set.X)
+    print(test_set.y)
