21313c4e121f5404e49b4dcf260dbde135665a7e
beach-fossils <jddmoura@gmail.com>
2022-12-22 21:21:07 +0000

    [ADD] com comentários

--- src/si/feature_selection/select_k_best.py
+++ src/si/feature_selection/select_k_best.py
@@ -1,12 +1,13 @@
 import numpy as np
 from si.data.dataset import Dataset
 from src.si.statistics import f_classification
+from typing import Callable
 
 
 # score_func – função de análise da variância (f_classification) -> usar no teste
 
 class SelectKBest:
-    def __init__(self, score_func, k):
+    def __init__(self, score_func: Callable = f_classification, k: int = 10):
         self.score_func = score_func
         self.k = k
         self.F = None
@@ -17,29 +18,18 @@ class SelectKBest:
         self.F, self.p = self.score_func(dataset)
         return self
 
-    def transform(self, dataset: Dataset):
-        """Returns the k best features."""
-        idxs = np.argsort(self.F)[-self.k:]  # como é crescente, vamos buscar ao contrário (-)
-        features = np.array(dataset.features)[idxs]
-        return Dataset(x=dataset.x[:, idxs], y=dataset.y, features=features, label=dataset.label)  # retorna um novo
-        # dataset com as features selecionadas
+    def transform(self, dataset: Dataset) -> Dataset:
+        """ Selects the k features with highest F values and returns the dataset with highest scoring features. """
+
+        idxs = np.argsort(self.F)[-self.k:]  # retorna os indices das k features com maior F, como é um vetor ordenado,
+        # usa-se o -k para ter os ultimos k elementos
+        features = np.array(dataset.features)[idxs]  # retorna um vetor com os nomes das features que passaram no
+        # threshold
+        return Dataset(X=dataset.X[:, idxs], y=dataset.y, features=list(features), label=dataset.label)
 
     def fit_transform(self, dataset: Dataset):
         self.fit(dataset)
         return self.transform(dataset)
 
 
-# testing the methods
-if __name__ == '__main__':
-    x = np.array([[1, 2, 3], [4, 5, 6]])
-    y = np.array([1, 2])
-    features = ['a', 'b', 'c']
-    label = 'y'
-    dataset = Dataset(x, y, features, label)
-    # print(dataset.summary())
 
-    # testing fit method
-    skb = SelectKBest(f_classification, k=1)
-    print(skb.fit(dataset).F)
-
-# avaliaçao: selcionar por maior F -> maior diferença nos dados
