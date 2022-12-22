d15527d81ebf2ba063b4977a11d36bd610631c0e
beach-fossils <jddmoura@gmail.com>
2022-12-22 21:44:34 +0000

    [ADD] SelectPercentile added with comentaries

--- src/si/feature_selection/select_percentile.py
+++ src/si/feature_selection/select_percentile.py
@@ -4,24 +4,32 @@ from si.data.dataset import Dataset
 
 
 class SelectPercentile:
+
     def __init__(self, score_func, percentile):
-        self.score_func = score_func # função de análise da variância (f_classification)
-        self.percentile = percentile # percentil de features a selecionar
+        self.score_func = score_func  # função de análise da variância (f_classification)
+        self.percentile = percentile  # percentil de features a selecionar
         self.F = None
         self.p = None
 
-    def fit(self, dataset: Dataset):
+    def fit(self, dataset):
         """Estimate the F and p values for each feature"""
         self.F, self.p = self.score_func(dataset)
         return self
 
-    def transform(self, dataset: Dataset):
-        """Returns the k best features."""
-        idxs = np.argsort(self.F)[-self.k:] # argsort -> ordena os valores e retorna os indices
-        features = np.array(dataset.features)[idxs]
-        return Dataset(x=dataset.x[:, idxs], y=dataset.y, features=features, label=dataset.label) # retorna um novo
+    def transform(self, dataset):
+        """Selects the k features compatible with the percentile desired to be applied on F values and returns the
+        dataset with the features that passed the percentile threshold. """
 
-    def fit_transform(self, dataset: Dataset):
+        # saber quais são as features que passam no threshold do percentil atrvés dos valores de F e ids
+        idxs = np.argsort(self.F)  # retorna os indices das features em ordem crescente de F
+
+        features = np.array(dataset.features)[idxs]  # retorna um vetor com os nomes das features em ordem crescente de
+        # F
+
+        return Dataset(X=dataset.X[:, idxs], y=dataset.y, features=list(features), label=dataset.label)
+
+    def fit_transform(self, dataset):
+        """Estimate the F and p values for each feature and returns the k best features."""
         self.fit(dataset)
         return self.transform(dataset)
 
