1aeb9a8295f6cb2bb4fa6ac2d0f6dc9556f58a53
beach-fossils <jddmoura@gmail.com>
2022-12-26 20:30:19 +0000

    [FIX] some issues were fixed

--- src/si/linear_model/ridge_regression.py
+++ src/si/linear_model/ridge_regression.py
@@ -27,6 +27,7 @@ class RidgeRegression:
         The model parameter, namely the intercept of the linear model.
         For example, theta_zero * 1
     """
+
     def __init__(self, l2_penalty: float = 1, alpha: float = 0.001, max_iter: int = 1000):
         """
 
@@ -48,7 +49,7 @@ class RidgeRegression:
         self.theta = None  # coeficientes/parametros do modelo para as variáveis de entrada (features)
         self.theta_zero = None  # coeficiente/parametro zero. também conhhecido como interseção
 
-    def fit(self, dataset: Dataset) -> 'RidgeRegression':
+    def fit(self, dataset: Dataset):
         """
         Fit the model to the dataset
 
@@ -65,29 +66,29 @@ class RidgeRegression:
         m, n = dataset.shape()
 
         # initialize the model parameters
-        self.theta = np.zeros(n) # theta = numero de features
+        self.theta = np.zeros(n)
         self.theta_zero = 0
 
-        # gradient descent
-        for i in range(self.max_iter):
-            # predicted y
-            y_pred = np.dot(dataset.X, self.theta) + self.theta_zero
+        # vetor de predições
+        y_pred = np.dot(dataset.X, self.theta) + self.theta_zero  # typical function y = mx + b
 
-            # computing and updating the gradient with the learning rate
-            gradient = (self.alpha * (1 / m)) * np.dot(y_pred - dataset.y, dataset.X)
+        # compute and update the gradient with learning rate
+        gradient = self.alpha
+        self.theta = self.theta - self.alpha * gradient
 
-            # computing the penalty
-            penalization_term = self.alpha * (self.l2_penalty / m) * self.theta
+        # calculate the penalty
+        penalty = self.alpha * (self.l2_penalty / m) * self.theta
 
-            # updating the model parameters
-            self.theta = self.theta - gradient - penalization_term
-            self.theta_zero = self.theta_zero - (self.alpha * (1 / m)) * np.sum(y_pred - dataset.y)
+        # update the parameters
+        self.theta = self.theta = self.theta - gradient - penalty
+        self.theta_zero = self.theta_zero - self.alpha * (1 / m) * np.sum(y_pred - dataset.y)
 
-        return self
+        # cost history
+        cost_history = np.zeros(self.max_iter)  # vetor de custo
 
     def predict(self, dataset: Dataset) -> np.array:
         """
-        Predict the output of the dataset
+        Estimates the values of y for the dataset using theta and theta zero.
 
         Parameters
         ----------
